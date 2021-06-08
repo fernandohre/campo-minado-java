35c51c6f0f115a410e4251ac8cb86e214b247264
fernandohre <senhor.clinton@gmail.com>
2021-06-08 00:40:04 -0300

    campo minado

---
+++ campo-minado/src/main/java/com/campo/minado/App.java
@@ -0,0 +1,14 @@
+package com.campo.minado;
+
+/**
+ * Campo minado
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Campo minado iniciado!" );
+        Jogo jogo = new Jogo();
+    }
+}
